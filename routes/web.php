17411f31c65d6800614148b7fb7efede2e4255bd
Angelo Salazar <uns.248@gmail.com>
2023-04-26 19:49:22 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/', 'RecordController@index');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', [RecordController::class, 'index']);
 Route::get('/records/create', 'RecordController@create');
 Route::post('/records', 'RecordController@store');
 Route::get('/records/{id}', 'RecordController@show');
